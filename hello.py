e47aabca48e9bf4a01f54845d4f2653bc65c2204
Orly Pantaleon <orlypantaleon25@gmail.com>
2025-03-24 14:13:42 +0800

    My first Python program

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello world!")
+
+name = "Orly Pantaleon"
+course = "Bachelor of Science in Information Technology"
+interests_in_programming = "Python and Cloud Computing"
+
+print(f"Name: {name}")
+print(f"Course: {course}")
+print(f"Interests in programming: {interests_in_programming}")
